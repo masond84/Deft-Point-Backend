277e04624d22b6e3ac0e661633fa1d0718037980
devin_mason <mvp@vastofi.com>
2025-03-08 17:12:37 -0500

    Update for vercel redeployment

--- server.js
+++ server.js
@@ -9,12 +9,13 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
+mongoose
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.error("MongoDB Connection Error:", err));
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.error("MongoDB Connection Error:", err));
 
 // Define Mongoose Schema
 const projectSchema = new mongoose.Schema({
